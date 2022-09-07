7483066d7e5fe09749a32c9a63ed2aaa172bc0a7
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2022-09-07 16:01:15 -0400

    adding parser class for easier parser results

--- yamlparser.pas
+++ yamlparser.pas
@@ -8,6 +8,7 @@ uses
 const
   ytkComma = ytkSeparator;
   ytkColon = ytkMapValue;
+  ytkQuestion = ytkMapKey;
   ytkDash = ytkSequence;
   ytkBracketOpen = ytkBlockOpen;
   ytkBracketClose = ytkBlockClose;
@@ -15,6 +16,24 @@ const
   ytkCurlyClose = ytkMapClose;
 
   ytkEndOfScan = [ytkEof, ytkEndOfDoc];
+
+  // valid tokens for start
+  DocStarters = [
+    ytkSequence
+   ,ytkMapKey
+   ,ytkMapValue
+   ,ytkSeparator
+   ,ytkBlockOpen
+   ,ytkMapStart
+   ,ytkAnchor
+   ,ytkAlias
+   ,ytkNodeTag
+   ,ytkLiteral
+   ,ytkFolded
+   ,ytkIdent
+   ,ytkStartOfDoc
+   ,ytkEndOfDoc // if we found this one, we should also report, as if the document got started
+  ];
 
 type
   TSetOfYamlTokens = set of TYamlToken;
@@ -61,8 +80,84 @@ type
 procedure SkipToNewline(sc: TYamlScanner);
 
 function ParseKeyScalar(sc: TYamlScanner): string;
-function SkipComments(sc: TYamlScanner): string;
-function SkipCommentsEoln(sc: TYamlScanner): string;
+procedure SkipComments(sc: TYamlScanner);
+procedure SkipCommentsEoln(sc: TYamlScanner);
+
+type
+  TYamlEntry = (
+    yeScalar,        // "scalar" is filled, "tag" is filled
+    yeScalarNull,
+    yeDocStart,
+    yeDocEnd,
+    yeKeyMapStart,   // "key" is not filled, "tag" is filled
+    yeKeyMapClose,
+    yeArrayStart,
+    yeArrayEnd,
+    yeNewDoc
+  );
+
+  TParseState = (
+    psError,         // an error occurred
+    psInit,           // before start of a document
+    psDetect,         // trying to figure out what value we're looking at
+    psReportKey,      // we've found a scalar, that seems to be the key, but so we need to report it
+    psReportStartDoc, // need to re-report the document start
+    psConsumeKeyFlow,  // the start of the map has been detected "[" consume key
+    psConsumeKeyBlock,  // the start of the map has been detected "{" consume key
+    psConsumeValue,   // we've reported the key, we need a value now
+    psEof
+
+  );
+
+  TParserContext = class(TObject)
+    isStarted  : Boolean; // has been reported as started or not;
+                          // if it was started, it must also be closed
+    indent     : integer; // the indentation used to start the context
+    isArray    : Boolean; // if true, then sequence, otherwise - map
+
+    vals       : Integer; // number of values
+    isKeyFound : Boolean; // the key was found (used for map)
+                          // if the key is not found, we must report "non-existing" value
+                          // but the context is finished
+    isFlow     : Boolean;
+    prev       : TParserContext;
+  end;
+
+  { TYamlParser }
+
+  TYamlParser = class(TObject)
+  protected
+    fState     : TParseState;
+    hasDoc     : Boolean;
+    ctx        : TParserContext;
+    root       : TParserContext;
+    function DoParseNext: Boolean;
+    procedure ResetContext;
+    function PopupContext: TYamlEntry;
+
+    procedure SwitchContext(isArray, isFlow: Boolean; var isNewContext: Boolean);
+
+    procedure ConsumeDirective(const dir: string); virtual;
+  public
+    scanner    : TYamlScanner;
+    ownScanner : Boolean;
+
+    tag        : string;
+    scalar     : string;
+    entry      : TYamlEntry;
+
+
+    errorMsg   : string;
+    errorLine  : Integer;
+    errorChar  : Integer;
+    errorState : TParseState;
+    constructor Create;
+    destructor Destroy; override;
+    procedure SetBuffer(const buf: string);
+
+    function ParseNext: Boolean;
+    property ParserState: TParseState read fState;
+  end;
 
 implementation
 
@@ -112,15 +207,301 @@ begin
     Result := '';
 end;
 
-function SkipComments(sc: TYamlScanner): string;
+procedure SkipComments(sc: TYamlScanner);
 begin
   if sc.token = ytkComment then sc.ScanNext;
 end;
 
-function SkipCommentsEoln(sc: TYamlScanner): string;
+procedure SkipCommentsEoln(sc: TYamlScanner);
 begin
   while sc.token in [ytkComment,ytkEoln] do
     sc.ScanNext;
+end;
+
+{ TYamlParser }
+
+constructor TYamlParser.Create;
+begin
+  inherited Create;
+  root := TParserContext.Create;
+end;
+
+destructor TYamlParser.Destroy;
+begin
+  ResetContext;
+  root.Free;
+  if ownScanner then scanner.Free;
+  inherited Destroy;
+end;
+
+procedure TYamlParser.SetBuffer(const buf: string);
+begin
+  if not Assigned(scanner) then begin
+    scanner := TYamlScanner.Create;
+    ownScanner := true;
+  end;
+  fState := psInit;
+  scanner.SetBuffer(buf);
+  scanner.ScanNext;
+  errorMsg   := '';
+  errorLine  := 0;
+  errorChar  := 0;
+  hasDoc := false;
+  ResetContext;
+end;
+
+function TYamlParser.ParseNext: Boolean;
+begin
+  try
+    if scanner = nil then begin
+      Result := false;
+      Exit;
+    end;
+    if fState = psError then begin
+      // we're in error. don't want to parse anything anymore
+      // call SetBuffer to reset the state
+      Result := false;
+      Exit;
+    end;
+    Result := DoParseNext;
+  except
+    on e: EYamlParserError do begin
+      errorMsg := e.Message;
+      errorLine := e.lineNum;
+      errorChar := e.charOfs;
+      errorState := fState;
+      Result := false;
+      fState := psError;
+    end;
+  end
+end;
+
+function TYamlParser.DoParseNext: Boolean;
+var
+  done : Boolean;
+  isNew : Boolean;
+begin
+  Result := false;
+
+  if entry in [yeScalar, yeArrayStart, yeKeyMapStart] then begin
+    // cleanup
+    tag := '';
+    if (entry = yeScalar) then scalar := '';
+  end;
+
+  repeat
+    done := true;
+    case fState of
+      psInit:
+      begin
+        SkipCommentsEoln(scanner);
+        if scanner.token = ytkDirective then begin
+          Result := true;
+          ConsumeDirective(scanner.text);
+          scanner.ScanNext;
+          SkipCommentsEoln(scanner);
+          done := false;
+        end else if scanner.token in DocStarters then begin
+          Result := true;
+          hasDoc := true;
+          entry := yeDocStart;
+          if scanner.token = ytkStartOfDoc then
+            scanner.ScanNext;
+          fState := psDetect;
+        end else
+          raise EYamlInvalidToken.Create(scanner);
+      end;
+
+      psDetect:
+      begin
+        SkipCommentsEoln(scanner);
+
+        // level down
+        if (ctx.isStarted) and (scanner.tokenIndent < ctx.indent) then begin
+          entry := PopupContext;
+          Result := true;
+        end else if scanner.token = ytkIdent then begin
+          scalar := ParseKeyScalar(scanner);
+          if scanner.token = ytkColon then begin
+            entry := yeKeyMapStart;
+            fState := psReportKey;
+            Result := true;
+          end else begin
+            entry := yeScalar;
+            Result := true;
+          end;
+        end else if scanner.token = ytkSequence then begin
+          SwitchContext(True, False, isNew);
+          if isNew then
+            entry := yeArrayStart
+          else
+            // we're at the same array. let's just read the next value
+            done := false;
+          scanner.ScanNext;
+          Result := true;
+        end else if scanner.token = ytkQuestion then begin
+          SwitchContext(false, False, isNew);
+          if isNew then
+            entry := yeKeyMapStart
+          else
+            done := false;
+          scanner.ScanNext;
+          fState := psConsumeKeyBlock;
+          Result := true;
+        end else if scanner.token = ytkNodeTag then begin
+          tag := scanner.text;
+          scanner.ScanNext;
+          done := false;
+        end else if scanner.token = ytkStartOfDoc then begin
+          Result := true;
+          if hasDoc then begin
+            fState := psReportStartDoc;
+            entry := yeDocEnd;
+          end else
+            entry := yeDocStart;
+          scanner.ScanNext;
+          hasDoc := true;
+        end else if scanner.token = ytkEof then begin
+          fState := psEof;
+          done := false;
+        end else
+          raise EYamlInvalidToken.Create(scanner);
+      end;
+
+      psReportKey:
+      begin
+        entry := yeScalar;
+        fState := psConsumeValue;
+        Result := true;
+      end;
+
+      psReportStartDoc: begin
+        entry := yeDocStart;
+        fState := psDetect;
+        Result := true;
+      end;
+
+      psConsumeKeyBlock, psConsumeKeyFlow: begin
+        if scanner.token = ytkIdent then begin
+          scalar := ParseKeyScalar(scanner);
+          entry := yeScalar;
+          fState := psConsumeValue;
+          Result := true;
+          Exit;
+
+        end else
+          raise EYamlInvalidToken.Create(scanner);
+      end;
+
+      psConsumeValue:
+      begin
+        Result := true;
+        SkipCommentsEoln(scanner);
+        if scanner.token = ytkColon then begin
+          scanner.ScanNext;
+          SkipCommentsEoln(scanner);
+        end else if scanner.token = ytkMapKey then begin
+          // we were expecting the value, but encountered MapKey.
+          // that MapKey can be the value (if it's SUB consume)
+          // OR, if it's the same, then it's a "null" value
+          if (ctx.isStarted) and (not ctx.isArray) and (scanner.tokenIndent = ctx.indent) then begin
+            entry := yeScalarNull;
+            fState := psDetect;
+          end else begin
+            // "psDetect" should be able to handle creating the context
+            done := false;
+            fState := psDetect;
+          end;
+        end else if scanner.token = ytkEof then begin
+          // we were expecting the value. But it's EOF now.
+          // let's report it as null, and shutdown everything
+          entry := yeScalarNull;
+          fState := psEof;
+        end else begin
+          // trying to parse the value
+          fState := psDetect;
+          done := false;
+        end;
+      end;
+
+      psEof:
+        if (ctx.isStarted) then begin
+          entry := PopupContext;
+          Result := true;
+        end else if not root.isStarted then begin
+          if hasDoc then begin
+            entry := yeDocEnd;
+            Result := true;
+            hasDoc := false;
+          end else
+            // we're done!
+            Result := false;
+        end;
+    end; // case of fState
+  until done;
+end;
+
+procedure TYamlParser.ResetContext;
+var
+  t: TParserContext;
+begin
+  while Assigned(ctx) and (ctx<>root) do begin
+    t:=ctx;
+    ctx := ctx.prev;
+    t.Free;
+  end;
+  root.isKeyFound := false;
+  root.isArray := false;
+  ctx:=root;
+end;
+
+function TYamlParser.PopupContext: TYamlEntry;
+var
+  t : TParserContext;
+begin
+  if ctx=root then begin
+    if root.isStarted then begin
+      root.isStarted := false;
+      if root.isArray then Result := yeArrayEnd
+      else Result := yeKeyMapClose;
+    end else
+      Result := yeScalar;
+    Exit;
+  end;
+  t := ctx;
+  ctx := ctx.prev;
+  if t.isArray then Result := yeArrayEnd
+  else Result := yeKeyMapClose;
+
+  t.Free;
+end;
+
+procedure TYamlParser.SwitchContext(isArray, isFlow: Boolean; var isNewContext: Boolean);
+var
+  n : TParserContext;
+  id : integer;
+begin
+  isNewContext := false;
+  id := scanner.tokenIndent;
+  isNewContext := (not ctx.isStarted)
+    or ((ctx.isStarted) and ((ctx.isArray <> isArray) or (ctx.indent <> id)));
+
+  if not (isNewContext) then Exit;
+  if ctx.isStarted  then begin
+    n := TParserContext.Create;
+    n.prev := ctx;
+    ctx := n;
+  end;
+
+  ctx.isStarted := true;
+  ctx.isArray := isArray;
+  ctx.isflow := isFlow;
+  ctx.indent := id;
+end;
+
+procedure TYamlParser.ConsumeDirective(const dir: string);
+begin
+
 end;
 
 { EYamlParserError }
