d051f0a0da332d842c1e3deeb5bc1b88c4c84b1c
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2022-09-07 16:37:24 -0400

    rewriting tojson parser to use yamlparser

--- tojsonparsersample.pas
+++ tojsonparsersample.pas
@@ -15,6 +15,7 @@ var
   sc  : TYamlScanner;
   j   : TJSONData;
   st  : TFPJSStream;
+  err : string;
 begin
   res := '';
   fs := TFileStream.Create(fn, fmOpenRead or fmShareDenyNone);
@@ -27,19 +28,26 @@ begin
   sc := TYamlScanner.Create;
   try
     sc.SetBuffer(res);
-    j := ParseToJson(sc);
 
-    st := TFPJSStream.Create;
-    try
-      j.DumpJSON(st);
-      SetLength(res, st.Size);
-      st.Position:=0;
-      if length(res)>0 then
-        st.REad(res[1], length(res));
-      writeln(res);
-    finally
-      st.Free;
+    j := ParseToJson(sc, DefaultOpts, err);
+    if err<>'' then begin
+      writeln('error: ', err);
     end;
+
+    if Assigned(j) then begin
+      st := TFPJSStream.Create;
+      try
+        j.DumpJSON(st);
+        SetLength(res, st.Size);
+        st.Position:=0;
+        if length(res)>0 then
+          st.REad(res[1], length(res));
+        writeln(res);
+      finally
+        st.Free;
+      end;
+    end else
+      writeln('none');
 
     j.Free;
   finally
--- yamlparsetojson.pas
+++ yamlparsetojson.pas
@@ -28,164 +28,83 @@ begin
   else Result := TJSONString.Create('');
 end;
 
+function ParseToJsonData(pr: TYamlParser; const opts: TYamlToJsonOptions): TJSONData; forward;
 
-// parsing { key:value, ... } flow
-procedure ParseKeyValueBlock(sc: TYamlScanner; const opts: TYamlToJsonOptions; dst: TJSONOBject); forward;
-procedure ParseToArray(sc: TYamlScanner; const opts: TYamlToJsonOptions; dst: TJSONArray; stayIdent: integer); forward;
-// tries to parse a value buy current status of the scanner
-function ParseToJsonInt(sc: TYamlScanner; const opts: TYamlToJsonOptions; indent: integer; const ExtraStops: TSetOfYamlTokens = []): TJSONData; forward;
-
-// This is an explicit Key/Value block. Expecting "}" to be reported
-procedure ParseKeyValueBlock(sc: TYamlScanner; const opts: TYamlToJsonOptions; dst: TJSONOBject);
-var
-  done : Boolean;
-  key  : string;
-  vl   : TJSONData;
-  suba : TJSONArray;
+procedure ParseToJsonArr(pr: TYamlParser; const opts: TYamlToJsonOptions; dst: TJSONArray);
 begin
-  if sc.token <> ytkCurlyOpen then Exit;
-  sc.ScanNext;
-
-  done := false;
-  while not done do begin
-    SkipCommentsEoln(sc);
-    key := ParseKeyScalar(sc);
-    SkipCommentsEoln(sc);
-    vl := nil;
-    if sc.token = ytkColon then begin
-      sc.ScanNext;
-      SkipCommentsEoln(sc);
-    end;
-
-    case sc.token of
-      ytkCurlyClose: begin
-        sc.ScanNext;
-        done := true;
-      end;
-      ytkComma: begin
-        sc.ScanNext;
-        SkipCommentsEoln(sc);
-        // the last comma after the last value is allowed
-        // { k:v, k2:v, }
-        if (sc.token = ytkCurlyClose) then begin
-          sc.ScanNext;
-          done := true;
-        end;
-      end;
-      ytkBracketOpen:  begin
-        suba := TJSONArray.Create();
-        ParseToArray(sc, opts, suba, sc.tokenIndent);
-        vl := suba;
-      end;
-    else
-      vl := ParseToJsonInt(sc, opts, sc.tokenIndent, [ytkCurlyClose, ytkComma]);
-    end;
-    if vl = nil then
-      vl := TJSONNull.Create;
-    dst.Add(key, vl);
+  while (pr.ParseNext) do begin
+    if pr.entry = yeArrayEnd then break;
+    dst.Add( ParseToJsonData(pr, opts));
   end;
 end;
 
-// sc is at "sequence" and everyone else here are expected to be sequences
-procedure ParseToArray(sc: TYamlScanner; const opts: TYamlToJsonOptions; dst: TJSONArray; stayIdent: integer);
+procedure ParseToJsonObj(pr: TYamlParser; const opts: TYamlToJsonOptions; dst: TJSONObject);
 var
-  tag  : string;
-  anch : string;
-  js   : TJSONData;
-  done : Boolean;
-  suba : TJSONArray;
+  k : TJSONData;
+  v : TJSONData;
 begin
-  if not (sc.token in [ytkComma, ytkDash, ytkBracketOpen]) then Exit;
-  sc.ScanNext;
-  done := false;
-  while not done do begin
-    if (sc.token = ytkDash) then begin
-      suba := TJSONArray.Create();
-      ParseToArray(sc, opts, suba, sc.tokenIndent);
-      dst.Add(suba);
+  if pr.entry <> yeKeyMapStart then
+    Exit;
 
-    end else begin
-      ParseTagAnchor(sc, tag, anch);
-
-      js := JsonValueFromTag(tag);
-
-      if sc.token = ytkIdent then begin
-        js.AsString := sc.GetValue;
-        dst.Add(js);
-      end;
-      SkipToNewline(sc);
-      if (sc.tokenIndent < stayIdent) or (sc.token in ytkEndOfScan) then begin
-        done := true
-      end else if sc.token in [ytkBlockClose] then begin
-        sc.ScanNext;
-        done := true;
-      end else if (sc.tokenIndent = stayIdent) and (sc.token in [ytkDash, ytkComma]) then begin
-        sc.ScanNext;
-      end;
-    end;
-  end;
+  repeat
+    k := ParseToJsonData(pr, opts);
+    pr.ParseNext;
+    v := ParseToJsonData(pr, opts);
+    pr.ParseNext;
+    dst.Add(k.AsString, v);
+  until pr.entry = yeKeyMapClose;
 end;
 
-function ParseToJsonInt(sc: TYamlScanner; const opts: TYamlToJsonOptions; indent: integer; const ExtraStops: TSetOfYamlTokens): TJSONData;
+function ParseToJsonData(pr: TYamlParser; const opts: TYamlToJsonOptions): TJSONData;
 var
-  tk : TYamlToken;
-
-  hasKey : Boolean;
-  obj    : TJSONObject;
-  key    : string;
-  arr    : TJSONArray;
- begin
+  dstObj : TJSONObject;
+  dstArr : TJSONArray;
+begin
   Result := nil;
-  hasKey := false;
-  obj := nil;
-  arr := nil;
-  while true do begin
-    SkipCommentsEoln(sc);
-    tk := sc.Token;
-    // out of the parent
-    if sc.tokenIndent < indent then break;
-    if tk = ytkError then break;
-    if tk = ytkEof then break;
-    if (ExtraStops <> []) and (tk in ExtraStops) then break;
-
-    if (tk = ytkStartOfDoc) then
-      // doing nothing about it
-    else if tk = ytkSequence then begin
-      arr := TJSONArray.Create;
-      if Result = nil then Result := arr;
-      ParseToArray(sc, opts, arr, sc.tokenIndent);
-    end else if tk=ytkCurlyOpen then begin;
-      obj := TJSONObject.Create;
-      if Result = nil then Result := obj;
-      ParseKeyValueBlock(sc, opts, obj);
-
-    end else if tk = ytkIdent then begin
-      if not hasKey then begin
-        key := sc.GetValue;
-        hasKey := true;
-      end else begin
-        if not Assigned (obj) then obj := TJSONObject.Create;
-        obj.add(key, sc.GetValue);
-
-        if not Assigned(result) then result := obj;
-        hasKey:=false;
+  while (pr.ParseNext) do begin
+    case pr.entry of
+      yeDocStart: begin
+        // todo: support multi-document yaml
       end;
-    end;
-
-    tk := sc.ScanNext;
-  end;
-
-  if not Assigned(Result) and hasKey then begin
-    Result := TJSONString.Create(key);
-  end;
-  if not Assigned(Result) then
-    Result := TJSONNull.Create;
+      yeKeyMapStart: begin
+        dstObj := TJSONObject.Create;
+        ParseToJsonObj(pr, opts, dstObj);
+        Result := dstObj;
+        break;
+      end;
+      yeArrayStart: begin
+        dstArr := TJSONArray.Create;
+        ParseToJsonArr(pr, opts, dstArr);
+        Result := dstArr;
+        break;
+      end;
+      yeScalarNull: begin
+        Result := TJSONNull.Create;
+        break;
+      end;
+      yeScalar:
+      begin
+        Result := JsonValueFromTag(pr.tag);
+        Result.AsString := pr.scalar;
+        break;
+      end;
+      else
+        break;
+    end; // of case
+  end; // of while
 end;
 
 function ParseToJson(sc: TYamlScanner; const opts: TYamlToJsonOptions = DefaultOpts): TJSONData;
+var
+  p : TYamlParser;
 begin
-  sc.ScanNext;
-  Result := ParseToJsonInt(sc, opts, sc.tokenIndent);
+  p := TYamlParser.Create;
+  try
+    p.SetScanner(sc, false);
+    Result := ParseToJsonData(p, opts);
+  finally
+    p.Free;
+  end;
 end;
 
 function ParseToJson(sc: TYamlScanner; const opts: TYamlToJsonOptions; out err: string): TJSONData;
