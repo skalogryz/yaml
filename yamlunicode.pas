7189a626437d69c8db56d2e56094710a5cb8a55c
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2022-09-10 17:13:50 -0400

    starting a unicode utility

---
+++ yamlunicode.pas
@@ -0,0 +1,15 @@
+unit yamlunicode;
+
+interface
+
+{$ifdef fpc}
+{$mode delphi}{$H+}
+{$define hasinline}
+{$endif}
+
+// the unicode related utilities
+// heavily depend on the compiler run-time
+
+implementation
+
+end.
